8e7bec79f260a5621d0083c96b3d94c41dd7e222
Ellen Gummesson <ellengummesson@gmail.com>
2012-11-04 12:10:57 +0100

    Redesign + new blog post.

--- projects/simple-jquery-scrolling/assets/scroll.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".scroll").click(function(event) {    
-    event.preventDefault();
-      $('body').animate({scrollTop: $(this.hash).offset().top}, 1500); // 1500 = 1500ms. Change it if you want the scrolling to be faster/slower.
-  });
-});
