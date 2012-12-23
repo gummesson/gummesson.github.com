7b24d421cee4433881b6bb6cec400551a0aaf7e6
Ellen Gummesson <ellengummesson@gmail.com>
2012-12-23 21:01:42 +0100

    Cleanup + updated blog post.

--- jobb/assets/jquery/scroll.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".scroll").click(function(event) {    
-    event.preventDefault();
-      $('body').animate({scrollTop: $(this.hash).offset().top}, 1250);
-  });
-});
